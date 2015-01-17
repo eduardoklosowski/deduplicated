e093999810f2af9e0695e8cd69a425a7cbc00223
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-01-16 23:28:55 -0200

    Create function sha1_file

--- deduplicated/__init__.py
+++ deduplicated/__init__.py
@@ -6,12 +6,25 @@
 
 from __future__ import unicode_literals
 
+from hashlib import sha1
 import os
 
 
 # Global Vars
 
 CACHE_DIR = os.path.join(os.path.expanduser('~'), '.deduplicated')
+
+
+# Utils
+
+def sha1_file(filename):
+    with open(filename, 'rb') as fp:
+        s = sha1()
+        block = True
+        while block:
+            block = fp.read(2 ** 10)
+            s.update(block)
+        return s.hexdigest()
 
 
 # Create user directory if not exists
